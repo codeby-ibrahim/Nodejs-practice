0db21bd15220427c797e0700dd139762dbe11761
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com>
2025-11-18 12:43:53 +0500

    added

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
 
 app.get('/', function (req, res) {
-    res.send("Chal rha hai!");
+    res.render("index");
 })
 
 app.listen(3000, function () {
