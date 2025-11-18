f601d0823b988d9f8c0ec85c66580ee953289ea4
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com>
2025-11-18 14:56:01 +0500

    added

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const path = require("path");
 
-app.get('/', (req, res){
-    res.send('Wellcome to the Home page');
-})
+// Set view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // VERY IMPORTANT
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Correct static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index"); // Must match index.ejs
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
