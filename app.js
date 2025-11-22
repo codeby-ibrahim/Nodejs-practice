0ce0b7402d9880a0a9798321a2efaaccfa50d235
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com>
2025-11-22 09:10:57 +0500

    added

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const app = express();
+const userModel = require('./usermodel');
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
-app.listen(3000);
+    res.send('Hello world!');
+});
+
+app.get('/create', async (req, res) => {
+    try {
+        let createdUser = await userModel.create({
+            name: 'ibrahim',
+            username: 'ibrahim123',
+            email: 'ibrahim123@gmail.com'
+        });
+
+        res.send(createdUser);
+    } catch (error) {
+        res.send(error);
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
