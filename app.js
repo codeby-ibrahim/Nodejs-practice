3b59e73e3b10f9c90b3c31ded6bcf152326498a1
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com>
2025-11-25 12:31:58 +0500

    added

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+
+// app.use("view engine", "ejs");
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join('__dirnmame', 'public')))
+// app
+
+
+// app.get('/', (req, res) => {
+//     res.render('hello.ejs');
+
+// })
+// app.listen(3000)
+
+
+
 const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use("view engine", "ejs");
+// Set view engine correctly
+app.set("view engine", "ejs");
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join('__dirnmame', 'public')))
-app
 
+// Correct static folder path (__dirname was wrong)
+app.use(express.static(path.join(__dirname, 'public')));
 
+// Route
 app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
-app.listen(3000)
+    res.render('index');   // No need for .ejs
+});
+app.get('/read', (req, res) => {
+    res.render('read');   // No need for .ejs
+});
+
+// Start server
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
